d32eeb5b8abe1e985b2faf4731ce291793f44f01
Longstick <1465914530@qq.com>
2022-08-11 03:04:46 +0800

    tkinter test

--- Frame.py
+++ Frame.py
@@ -1,28 +1,39 @@
+import sys
 from tkinter import *
 from tkinter import messagebox
 from tkinter import ttk
 
-def buttonpush():
-    messagebox.showinfo(title='msg', message='HAHA, that is GOOD')
-    
-
-top = Tk()
-top.title("test")
-top.geometry('500x500') 
-
-Button_1 = Button(top, text='Button 1', command=buttonpush)
-Button_2 = Button(top, text='Button 2')
-spbox_1 = ttk.Spinbox(
-    top, 
-    text='spbox 1', 
-    from_=0, 
-    to=20,
-    width=5
-    )
-
-Button_1.pack()
-Button_2.pack()
-spbox_1.pack()
 
 
-top.mainloop()
+class GUIFrame():
+
+    def __init__(self):
+        self.frame = Tk()
+        self.init_window()
+        
+        self.frame.mainloop()
+
+    def init_window(self):
+        # window
+        self.frame.title('hehe')
+        self.frame.geometry("400x400")
+        self.frame.resizable(0, 0)
+
+        # text
+        self.titleText = Text(self.frame, text="PointCounter")
+        self.titleText.grid(row = 1, column = 1)
+
+        # button
+        self.Button_1 = Button(self.frame, text="button 1")
+        self.Button_2 = Button(self.frame, text="button 2")
+
+        self.Button_1.grid(row = 0, column = 0)
+        self.Button_2.grid(row = 0, column = 1)
+
+    def buttonpush():
+        messagebox.showinfo(title='msg', message='HAHA, that is GOOD')
+
+def gui_start():
+    sysFrame = GUIFrame()
+
+gui_start()
